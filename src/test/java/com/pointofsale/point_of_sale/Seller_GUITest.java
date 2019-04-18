3460f6a5217aef61cd0fb9aa219778b8d22b2508
Bartlomiej Karbownik <bartlomiej.karbownik@gmail.com>
2019-04-18 12:35:37 +0200

    Upgraded tests

--- src/test/java/com/pointofsale/point_of_sale/Seller_GUITest.java
+++ src/test/java/com/pointofsale/point_of_sale/Seller_GUITest.java
@@ -5,6 +5,9 @@
  */
 package com.pointofsale.point_of_sale;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
@@ -13,6 +16,7 @@ import org.junit.Test;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
+import org.junit.BeforeClass;
 
 /**
  * Simple tests for <code>Seller_GUI</code>.
@@ -20,17 +24,43 @@ import java.text.DecimalFormat;
 
 public class Seller_GUITest{
 
-    private Seller_GUI seller = new Seller_GUI();
+    private Seller_GUI seller;
     private ConsoleOutputCapturer cos = new ConsoleOutputCapturer();
     private ResultSet rs;
     private long wrongId = 1010101055656L;
     private long rightId = 1234567890123L;
-
+    private Robot robot;
+    
     @Before
     public void setUp() throws SQLException {
+        seller = new Seller_GUI();
+        /*
         seller.listOfArticles.add(new Article(123L, "Some", 0.12));
         seller.listOfArticles.add(new Article(456L, "Thing", 3.45));
         seller.listOfArticles.add(new Article(789L, "Here", 6.78));
+        
+        try {
+         robot = new Robot();   
+        } catch(AWTException e) {
+            System.out.println("What is this robot doing?!");
+            e.printStackTrace();
+        }
+        */
+    }
+    
+    @Test
+    public void testAddToArticleList() {
+        seller.enterCode("123");
+        Article ar = seller.getAddedArticle();
+        assertEquals("Kajzerka", ar.getName());
+        assertEquals(0.20, ar.getCost(), 0.01);
+    }
+    
+    @Test
+    public void testTotalSum() {
+        seller.enterCode("123");
+        seller.enterCode("789");
+        assertEquals("0,79", seller.getSumTextArea());
     }
 
 /*
